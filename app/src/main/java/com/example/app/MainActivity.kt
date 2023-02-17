1d6aa4bfb539ba20a7e6ef4bea17e8587dc2004e
galihagussetyawan <galihdevelopment@gmail.com>
2023-02-17 11:33:01 +0700

    refactor: configuration toolbar

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -6,23 +6,20 @@ import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.app.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val toolbar = binding.toolbar
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
 
         setupNav()
     }
@@ -32,6 +29,16 @@ class MainActivity : AppCompatActivity() {
         val navController: NavController = findNavController(R.id.nav_host_fragment)
 
         bottomNavigationView.setupWithNavController(navController)
+
+//        appbar
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home
+        ))
+
+        val toolbar = binding.toolbar
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        setSupportActionBar(toolbar)
     }
 
 }
